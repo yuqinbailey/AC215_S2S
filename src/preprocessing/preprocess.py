12f01d523b19cf34f0a846db9d77e401cde7c526
Tiantong Li <tiantongli@Tiantongs-MBP.hsd1.ma.comcast.net>
2023-09-23 22:33:01 -0400

    update preprocess.py

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,1 +1,18 @@
-# preprocess.py
+import os
+from csv import reader
+from moviepy.editor import VideoFileClip
+
+def cut_video():
+    
+    os.makedirs('processed_video', exist_ok=True)
+    video_files = os.listdir('raw_video')
+
+    file = open('vggsound.csv',"r")
+    lines = list(reader(file))
+    start_times = {x[0]:int(x[1]) for x in lines}
+
+    for video_file in video_files:
+        clip = VideoFileClip(os.path.join('raw_video',video_file))
+        video_id = video_file.split('.')[0]
+        trimmed_clip = clip.subclip(start_times[video_id],start_times[video_id]+10)
+        trimmed_clip.write_videofile(os.path.join('processed_video',video_file))
