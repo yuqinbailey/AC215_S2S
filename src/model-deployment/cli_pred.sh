47ab32ad47dd93b5701ccecf242628fdb4283026
frankli <tiantongli@g.harvard.edu>
2023-10-24 02:17:47 -0400

    update image

--- src/model-deployment/cli_pred.sh
+++ src/model-deployment/cli_pred.sh
@@ -1,2 +1,6 @@
 CUSTOM_PREDICTOR_IMAGE_URI="gcr.io/ac215project-398818/pytorch_predict_regnet"
-docker build --tag=$CUSTOM_PREDICTOR_IMAGE_URI .
+# docker build --tag=$CUSTOM_PREDICTOR_IMAGE_URI .
+# docker run -t -d --rm -p 7080:7080 $CUSTOM_PREDICTOR_IMAGE_URI
+gcloud auth login
+gcloud auth configure-docker
+docker push $CUSTOM_PREDICTOR_IMAGE_URI
