fd38e4f678dbd0afee784da0239ea9a49455af6b
Yong Zhang <leocheung1001@yahoo.com>
2023-11-09 00:06:28 -0500

    Local testing done.

--- src/model_deployment/docker-shell_pred.sh
+++ src/model_deployment/docker-shell_pred.sh
@@ -1,7 +1,12 @@
-CUSTOM_PREDICTOR_IMAGE_URI="gcr.io/ac215project-398818/pytorch_predict_regnet"
-# docker build -f Dockerfile_pred --tag=$CUSTOM_PREDICTOR_IMAGE_URI .
-# docker run -t -d --rm -p 7080:7080 $CUSTOM_PREDICTOR_IMAGE_URI
-docker run -it gcr.io/ac215project-398818/pytorch_predict_regnet /bin/bash
+CUSTOM_PREDICTOR_IMAGE_URI="leo/deploy_regnet"
+docker build -f Dockerfile_pred --tag=$CUSTOM_PREDICTOR_IMAGE_URI .
+
+# Leo
+# docker run -ti --rm -p 7080:7080 gcr.io/ac215project-398818/pytorch_predict_regnet
+# docker run -ti --rm -p 7080:7080 gcr.io/ac215project-398818/pytorch_predict_regnet  /bin/bash
+
+# Frank
+# docker run -it gcr.io/ac215project-398818/pytorch_predict_regnet /bin/bash
 
 # gcloud auth login
 # gcloud auth configure-docker
